18992a67117f5e2f94c368ed8b75e6a4fa9dac4a
Shilpi Tamrakar <shilpitamrakar06@gmail.com>
2026-01-15 01:31:46 +0530

    first commit

---
+++ what is sql.sql
@@ -0,0 +1,21 @@
+# what is sql? 
+# sql is stand for {stractured query lanugage} it is a programming language  used to intract with relational databse 
+#   it is used for CRUD operation |
+# c- create  
+# R - read 
+# u - update 
+# D - delete 
+
+#Why SQL is used??
+
+#SQL is used to:
+
+#1.Store data in databases
+
+#2.Get data from databases
+
+#3.Update data
+
+#4.Delete data
+
+#5.Create and manage database tables
