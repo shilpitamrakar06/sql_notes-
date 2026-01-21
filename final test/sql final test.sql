cbbcc1e79f1044d5347f9ed905308a4bd4bbb247
Shilpi Tamrakar <shilpitamrakar06@gmail.com>
2026-01-21 22:37:45 +0530

    update

--- final test/sql final test.sql
+++ final test/sql final test.sql
@@ -69,22 +69,35 @@ select first_name ,last_name enrollment_id from students  s left join enrollment
 #18. Write a query to retrieve the first and last names of all students along with their
 #corresponding enrollment IDs using a RIGHT JOIN. Include enrollments that do not have
 #corresponding students.
-
+select first_name , last_name ,enrollment_id  from students s right join enrollments e on s.student_id = e.student_id ;
 #19. Write a query to retrieve the first and last names of all students along with their
 #corresponding enrollment IDs using an INNER JOIN.
+select first_name , last_name ,enrollment_id from students s inner join enrollments e on s.student_id = e.student_id ;
 #20. Retrieve the first and last names of all students along with their enrolled course names.
+select first_name ,last_name , enrollment_id ,course_name from students s join enrollments e on s.student_id = e.student_id 
+join courses c on c.course_id = e.course_id ;
 #21. Retrieve the list of students along with their enrollment dates. Include students who have
 #not enrolled in any courses.
+ select first_name, last_name, enrollment_date  from students s
+ left join enrollments e on s.student_id = e.student_id ;
 #22. List the names of students who have not enrolled in any courses.
+select first_name, last_name ,enrollment_date  from students s
+ left join enrollments e on s.student_id = e.student_id where e.enrollment_id is null ; 
 #23. Retrieve the first name, last name, and course name for all students enrolled in courses
 #taught by &#39;Dr. Smith&#39;.
+select first_name , last_name ,course_name from students s join enrollments e 
+on s.student_id = e.student_id  join courses c on c.course_id = e.course_id where instructor = "dr. smith";
 #24. List all students who are aged 18 or older and have enrolled in at least one course.
+select * from students s join enrollments  e on s.student_id = e.student_id join courses c on
+c.course_id = e.course_id where age>=18 ;
+
 #25. Count how many rows fetched when using left join, right join, inner join , cross join,
 #natural join
-Table 1 Table 2
-1 1
-1 1
-1 2
-2 2
-3 4
-3 null
+#Table 1  Table 2
+1            1               inner join  8 rows 
+1             1               left join 11 rows
+1              2            right join 10 rows 
+2              2              cross join 42 rows 
+3             4                natural join 8 rows 
+3             null
+3
